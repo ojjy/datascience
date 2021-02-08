ef9de5397622baf4b8c63cff88bc7c8fa102c918
yjjo <ojjy90@gmail.com>
2021-02-08 22:55:22 +0900

    add comment

--- cabbage/flask/app.py
+++ cabbage/flask/app.py
@@ -7,23 +7,24 @@ import numpy as np
 
 
 app = Flask(__name__)
-
+# place holder로 값을 넣는다
 X = tf.placeholder(tf.float32, shape=[None, 4])
 Y = tf.placeholder(tf.float32, shape=[None, 1])
+# y=ax+b에서 w는 a
 W = tf.Variable(tf.random_normal([4,1], name="weight"))
 b = tf.Variable(tf.random_normal([1]), name="bias")
 
-
+# 가설에 넣어 검증
 hypothesis = tf.matmul(X, W)+b
 
-
+# 그래프 빌드하고 세션을 만들어 이 세션을 실행시킴
 saver = tf.train.Saver()
 model = tf.global_variables_initializer()
-
 
 sess = tf.Session()
 sess.run(model)
 
+# checkpoint file
 sava_path = "./model/saved.cpkt"
 sava_path(sess, sava_path)
 
