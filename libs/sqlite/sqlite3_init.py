0951d21b880cccfd6070aaf182601d735468984c
yjjo <ojjy90@gmail.com>
2021-03-11 21:37:19 +0900

    add sqlite op

--- libs/sqlite/sqlite3_init.py
+++ libs/sqlite/sqlite3_init.py
@@ -10,12 +10,11 @@ def init_connection(dbf):
     return conn
 
 def create_table(conn):
-    sql = '''
-          CREATE TABLE IF NOT EXISTS test_tb(
-          id integer PRIMARY KEY,
-          name text
-          );
-          '''
+    sql = """CREATE TABLE IF NOT EXISTS test_tb(
+            id integer PRIMARY KEY,
+            name text
+            );
+          """
     try:
         c = conn.cursor()
         c.execute(sql)
@@ -24,9 +23,7 @@ def create_table(conn):
         print(e)
 
 def insert_one_row(conn, insert_vals):
-    sql = ''' 
-          INSERT INTO test_tb(id, name) VALUES(?,?)
-          '''
+    sql = """ INSERT INTO test_tb(id, name) VALUES(?,?)"""
     cur = conn.cursor()
     cur.execute(sql, insert_vals)
     conn.commit()
@@ -34,10 +31,7 @@ def insert_one_row(conn, insert_vals):
     return cur.lastrowid
 
 def select_all_rows(conn):
-    sql = """
-    SELECT * from test_tb
-    """
-    print(sql, " execution")
+    sql = """SELECT * from test_tb"""
     cur = conn.cursor()
     cur.execute(sql)
     rows = cur.fetchall()
@@ -47,23 +41,26 @@ def select_all_rows(conn):
     sql_print(sql)
 
 def delete_all_rows(conn):
-    sql = """
-    DELETE FROM test_tb
-    """
+    sql = """DELETE FROM test_tb"""
     cur = conn.cursor()
     cur.execute(sql)
     conn.commit()
     sql_print(sql)
     return conn
 
-
 def drop_one_table(conn):
-    sql = """
-    DROP TABLE test_tb
-    """
+    sql = """DROP TABLE test_tb"""
 
     cur = conn.cursor()
     cur.execute(sql)
+    conn.commit()
+    sql_print(sql)
+    return conn
+
+def update_one_row(conn, update_vals):
+    sql = """UPDATE test_tb SET id = ?, name = ? WHERE id =? """
+    cur = conn.cursor()
+    cur.execute(sql, update_vals)
     conn.commit()
     sql_print(sql)
     return conn
@@ -82,12 +79,13 @@ if __name__ == "__main__":
         print("Error")
 
     with conn:
-        for num in range(1, 10):
-            insert_vals = (num, "row num "+str(num))
+        for num in range(1, 20):
+            insert_vals=(num, "row num "+str(num))
             insert_one_row(conn, insert_vals)
 
         select_all_rows(conn)
-
+        update_one_row(conn, (101, "row num 10 update", 10))
+        select_all_rows(conn)
         delete_all_rows(conn)
         select_all_rows(conn)
 
---
+++ libs/sqlite/sqlite3_join.py
@@ -0,0 +1,54 @@
+"""
+student
+student_num
+student_name
+dept_id
+student_type: 0 - alumni , 1 - enrolled students
+adm_date
+
+dept
+dept_id
+dept_name
+
+sql statements
+
+CREATE TABLE IF NOT EXISTS dept(
+    dept_id INT,
+    dept_name TEXT,
+    PRIMARY KEY(dept_id)
+);
+
+CREATE TABLE IF NOT EXISTS student(
+    student_num INT,
+    student_name TEXT NOT NULL,
+    dept_id TEXT,
+    student_type INTEGER DEFAULT 0,
+    adm_date DATE,
+    PRIMARY KEY (student_num),
+    FOREIGN KEY (dept_id)
+        REFERENCES dept(dept_id)
+        ON DELETE CASCADE
+        ON UPDATE NO ACTION
+);
+
+# Multiple row insertion
+INSERT INTO dept (dept_id, dept_name) VALUES ("Computer Science"),
+                                             ("Mechanical Engineer"),
+                                             ("Medicine),
+                                             ("Piano"),
+                                             ("Economy"),
+                                             ("History"),
+                                             ("Law")
+
+# Insert into Select
+CREATE TABLE IF NOT EXISTS dept_backup(
+    dept_id INTEGER AUTOINCREMENT,
+    dept_name TEXT,
+    PRIMARY KEY(dept_id)
+);
+
+INSERT INTO dept_backup
+SELECT dept_id, dept_name FROM dept;
+
+SELECT * FROM dept_backup;
+"""
