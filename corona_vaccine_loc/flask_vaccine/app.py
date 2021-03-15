ff8ef38c9541e8db1c1b8b7f2d53f46235178820
yjjo <ojjy90@gmail.com>
2021-03-15 22:26:54 +0900

    add mysql

--- corona_vaccine_loc/flask_vaccine/app.py
+++ corona_vaccine_loc/flask_vaccine/app.py
@@ -1,18 +1,18 @@
 from flask import Flask
-import pandas as pd
-from sqlalchemy import create_engine
-import pymysql
 
 
-conn = pymysql.connect(host='127.0.0.1', user='test', password='password')
-cursor = conn.cursor()
 
-app = Flask(__name__)
+app = Flask(__name__)    # if conn is not None:
+
 
 @app.route('/')
 def hello_world():
+
     return 'Hello World!'
 
 
 if __name__ == '__main__':
+
+
+
     app.run()
